3efd9a1c9ed255846dfaa596059f7ec0e69217cd
Mounika P <mounikapotladurthiofficial@gmail.com>
2024-05-31 16:16:20 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState, useTransition } from 'react';
+import axios from 'axios';
 
 function App() {
+  const[data,setData]=useState([]);
+  let response;
+  useEffect(()=>{
+    axios.get("https://fakestoreapi.com/products").
+    then(response=>setData(response.data)).
+    catch(error=>console.log("error",error))
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <ul>
+          { data.map((item,index)=>(
+          <li key={index}>
+            {item.title}
+          </li>))}
+        </ul>
+      </center>
     </div>
   );
 }
